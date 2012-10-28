ce63fd63ff3ecfa3e1f8154f3fd566fbfeda8680
TJ Holowaychuk <tj@vision-media.ca>
2012-10-28 03:05:17 -0700

    faster

--- index.js
+++ index.js
@@ -1,132 +1,62 @@
 
-/**
- * Notation:
- * 
- *   - `t` current time in ms
- *   - `b` start value
- *   - `c` distance relative to start value
- *   - `d` duration in ms
- * 
- */
-
-exports.linear = function(t, b, c, d){
-  return c * t / d + b;
+exports.linear = function(n){
+  return n;
 };
 
-exports.inQuad = function (t, b, c, d) {
-  t /= d;
-  return c * t * t + b;
+exports.inQuad = function(n){
+  return n * n;
 };
 
-exports.outQuad = function (t, b, c, d) {
-  t /= d;
-  return -c * t * (t - 2) + b;
+exports.outQuad = function(n){
+  return n * (2 - n);
 };
 
-exports.inOutQuad = function (t, b, c, d) {
-  t /= d / 2;
-  if (t < 1) return c / 2 * t * t + b;
-  t--;
-  return -c / 2 * (t * (t - 2) - 1) + b;
+exports.inOutQuad = function(n){
+  n *= 2;
+  if (n < 1) return 0.5 * n * n;
+  return - 0.5 * (--n * (n - 2) - 1);
 };
 
-exports.inCube = function(t, b, c, d) {
-  t /= d;
-  return c*t*t*t + b;
+exports.inCube = function(n){
+  return n * n * n;
 };
 
-exports.outCube = function(t, b, c, d) {
-  t /= d;
-  t--;
-  return c*(t*t*t + 1) + b;
+exports.outCube = function(n){
+  return --n * n * n + 1;
 };
 
-exports.inOutCube = function(t, b, c, d) {
-  t /= d/2;
-  if (t < 1) return c / 2 * t * t * t + b;
-  t -= 2;
-  return c/2*(t*t*t + 2) + b;
+exports.inOutCube = function(n){
+  n *= 2;
+  if (n < 1) return 0.5 * n * n * n;
+  return 0.5 * ((n -= 2 ) * n * n + 2);
 };
 
-exports.inQuart = function(t, b, c, d) {
-  t /= d;
-  return c * t * t * t * t + b;
+exports.inQuart = function(n){
+  return n * n * n * n;
 };
 
-exports.outQuart = function(t, b, c, d) {
-  t /= d;
-  t--;
-  return -c * (t * t * t * t - 1) + b;
+exports.outQuart = function(n){
+  return 1 - (--n * n * n * n);
 };
 
-exports.inOutQuart = function(t, b, c, d) {
-  t /= d/2;
-  if (t < 1) return c / 2 * t * t * t * t + b;
-  t -= 2;
-  return -c / 2 * (t * t * t * t - 2) + b;
+exports.inOutQuart = function(n){
+  n *= 2;
+  if (n < 1) return 0.5 * n * n * n * n;
+  return -0.5 * ((n -= 2) * n * n * n - 2);
 };
 
-exports.inQuint = function(t, b, c, d) {
-  t /= d;
-  return c * t * t * t * t * t + b;
-};
+exports.inQuint = function(n){
+  return n * n * n * n * n;
+}
 
-exports.outQuint = function(t, b, c, d) {
-  t /= d;
-  t--;
-  return c * (t * t * t * t * t + 1) + b;
-};
+exports.outQuint = function(n){
+  return --n * n * n * n * n + 1;
+}
 
-exports.inOutQuint = function(t, b, c, d) {
-  t /= d / 2;
-  if (t < 1) return c / 2 * t * t * t * t * t + b;
-  t -= 2;
-  return c / 2 * (t * t * t * t * t + 2) + b;
-};
-
-exports.inSine = function(t, b, c, d) {
-  return -c * Math.cos(t / d * (Math.PI / 2)) + c + b;
-};
-
-exports.outSine = function(t, b, c, d) {
-  return c * Math.sin(t / d * (Math.PI / 2)) + b;
-};
-
-exports.inOutSine = function(t, b, c, d) {
-  return -c / 2 * (Math.cos(Math.PI * t / d) - 1) + b;
-};
-
-exports.inExpo = function(t, b, c, d) {
-  return c * Math.pow(2, 10 * (t / d - 1)) + b;
-};
-
-exports.outExpo = function(t, b, c, d) {
-  return c * (-Math.pow(2, -10 * t/d) + 1) + b;
-};
-
-exports.inOutExpo = function(t, b, c, d) {
-  t /= d / 2;
-  if (t < 1) return c / 2 * Math.pow(2, 10 * (t - 1)) + b;
-  t--;
-  return c / 2 * (-Math.pow(2, -10 * t) + 2) + b;
-};
-
-exports.inCirc = function(t, b, c, d) {
-  t /= d;
-  return -c * (Math.sqrt(1 - t * t) - 1) + b;
-};
-
-exports.outCirc = function(t, b, c, d) {
-  t /= d;
-  t--;
-  return c * Math.sqrt(1 - t * t) + b;
-};
-
-exports.inOutCirc = function(t, b, c, d) {
-  t /= d / 2;
-  if (t < 1) return -c/2 * (Math.sqrt(1 - t*t) - 1) + b;
-  t -= 2;
-  return c / 2 * (Math.sqrt(1 - t * t) + 1) + b;
+exports.inOutQuint = function(n){
+  n *= 2;
+  if (n < 1) return 0.5 * n * n * n * n * n;
+  return 0.5 * ((n -= 2) * n * n * n * n + 2);
 };
 
 // aliases
